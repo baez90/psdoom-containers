890b3bef4d54c5c5a2a9c6f9aa7b2474b21ec6fc
Peter Kurfer <peter.kurfer@googlemail.com>
2019-03-16 17:41:53 +0100

    feat(scripts): added script to spawn containers

--- scripts/psdoom-docker.sh
+++ scripts/psdoom-docker.sh
@@ -3,6 +3,7 @@
 export PSDOOMPSCMD="psdoom-containers docker ps"
 export PSDOOMRENICECMD="true"
 export PSDOOMKILLCMD="psdoom-containers docker kill"
+
 export DOOMWADPATH=/usr/share/games/doom
 
-psdoom-ng -window -merge -nosound -nosfx -nomusic
+psdoom-ng -window -merge -nosound -nosfx -nomusic -nograbmouse -devparm
--- scripts/spawn-containers.sh
+++ scripts/spawn-containers.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+for (( i = 0; i < 20; ++i )); do
+    docker run --rm -d --name "alpine_$i" alpine:latest /bin/ash -c 'sleep 3600';
+done
